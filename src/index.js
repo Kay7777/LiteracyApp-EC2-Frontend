5fdd8200b4739a8c5bf143b80e411d72a70af2d9
Kay <kay7song@gmail.com>
2020-12-12 08:06:54 -0700

    config security issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+"use strict"
 import App from "./App";
 import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
